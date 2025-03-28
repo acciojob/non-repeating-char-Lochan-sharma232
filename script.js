5b0024ffc2f9f13e9bcc82c31af37675ce40162e
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-28 13:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(str) 
+{
+	var ReapeatedChar;
+	for(var i=0;i<str.length;i++){
+		for(j=0;j<str.length;j++){
+			if(str[i]!=str[j]){
+				ReapeatedChar=str[j]
+			}else{
+				return null;
+			}
+		}
+	}
+	return ReapeatedChar;
  // Write your code here
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
